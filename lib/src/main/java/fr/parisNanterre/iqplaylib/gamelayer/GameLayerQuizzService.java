4918549b34c3826a8e260a63bf81e5b0589e9903
Sami Letellier <42002121@parisnanterre.fr>
2025-03-16 06:41:53 +0100

    Definition des signatures des méthodes pour le QuizzService. Implémentation de l'interface

--- lib/src/main/java/fr/parisNanterre/iqplaylib/gamelayer/GameLayerQuizzService.java
+++ lib/src/main/java/fr/parisNanterre/iqplaylib/gamelayer/GameLayerQuizzService.java
@@ -1,4 +1,33 @@
 package fr.parisnanterre.iqplaylib.gamelayer;
 
-public class GameLayerQuizzService {
+import fr.parisnanterre.iqplaylib.gamelayer.api.IGameLayerQuizzService;
+
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+
+public class GameLayerQuizzService extends GameLayerService implements IGameLayerQuizzService {
+    @Override
+    public HttpResponse getQuizzById(String quizz, String account) {
+        return null;
+    }
+
+    @Override
+    public HttpResponse getQuizzes(String account) {
+        return null;
+    }
+
+    @Override
+    public HttpResponse getResultOfQuizz(String quizz, String account) {
+        return null;
+    }
+
+    @Override
+    public HttpResponse startAQuizz(String quizz, String account, String player) {
+        return null;
+    }
+
+    @Override
+    public HttpResponse completeQuizz(String quizz, String account, String player, ArrayList<String> answers) {
+        return null;
+    }
 }
--- lib/src/main/java/fr/parisNanterre/iqplaylib/gamelayer/api/IGameLayerQuizzService.java
+++ lib/src/main/java/fr/parisNanterre/iqplaylib/gamelayer/api/IGameLayerQuizzService.java
@@ -1,4 +1,12 @@
 package fr.parisnanterre.iqplaylib.gamelayer.api;
 
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+
 public interface IGameLayerQuizzService {
+    HttpResponse getQuizzById(String quizz, String account);
+    HttpResponse getQuizzes(String account);
+    HttpResponse getResultOfQuizz(String quizz, String account);
+    HttpResponse startAQuizz(String quizz, String account, String player);
+    HttpResponse completeQuizz(String quizz, String account, String player, ArrayList<String> answers);
 }
