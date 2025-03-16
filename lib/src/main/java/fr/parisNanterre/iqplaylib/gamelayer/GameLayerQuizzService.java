1cfa9c14991764a5c914ddaf6375ce7db4e453f9
Sami Letellier <42002121@parisnanterre.fr>
2025-03-16 06:56:22 +0100

    Implémentation des méthodes getQuizzById, getQuizzes, getQuizzesByPlayer, getResultOfQuizz, getResultOfQuizzByPlayer du service GameLayerQuizzService

--- lib/src/main/java/fr/parisNanterre/iqplaylib/gamelayer/GameLayerQuizzService.java
+++ lib/src/main/java/fr/parisNanterre/iqplaylib/gamelayer/GameLayerQuizzService.java
@@ -2,23 +2,131 @@ package fr.parisnanterre.iqplaylib.gamelayer;
 
 import fr.parisnanterre.iqplaylib.gamelayer.api.IGameLayerQuizzService;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 
 public class GameLayerQuizzService extends GameLayerService implements IGameLayerQuizzService {
     @Override
-    public HttpResponse getQuizzById(String quizz, String account) {
-        return null;
+    public HttpResponse getQuizzById(String quizz, String account) throws IOException, InterruptedException {
+        String encodedQuizz = URLEncoder.encode(quizz, "UTF-8");
+        String encodedAccount = URLEncoder.encode(account, "UTF-8");
+
+        String fullUrl = API_URL + "/api/v0/quizzes/"
+                + encodedQuizz
+                + "?account=" + encodedAccount;
+
+        HttpResponse response = httpClient.send(
+                HttpRequest.newBuilder()
+                        .uri(URI.create(fullUrl))
+                        .headers(
+                                "api-key", API_KEY
+                        )
+                        .GET()
+                        .build(),
+                HttpResponse.BodyHandlers.ofString()
+        );
+        super.logInformation("Status Code: " + response.statusCode());
+        super.logInformation("Response Body: " + response.body());
+        return response;
     }
 
     @Override
-    public HttpResponse getQuizzes(String account) {
-        return null;
+    public HttpResponse getQuizzes(String account) throws IOException, InterruptedException {
+        String encodedAccount = URLEncoder.encode(account, "UTF-8");
+
+        String fullUrl = API_URL + "/api/v0/quizzes" + "?account=" + encodedAccount;
+
+        HttpResponse response = httpClient.send(
+                HttpRequest.newBuilder()
+                        .uri(URI.create(fullUrl))
+                        .headers(
+                                "api-key", API_KEY
+                        )
+                        .GET()
+                        .build(),
+                HttpResponse.BodyHandlers.ofString()
+        );
+        super.logInformation("Status Code: " + response.statusCode());
+        super.logInformation("Response Body: " + response.body());
+        return response;
     }
 
     @Override
-    public HttpResponse getResultOfQuizz(String quizz, String account) {
-        return null;
+    public HttpResponse getQuizzesByPlayer(String player, String account) throws IOException, InterruptedException {
+        String encodedAccount = URLEncoder.encode(account, "UTF-8");
+        String encodedPlayer = URLEncoder.encode(player, "UTF-8");
+
+        String fullUrl = API_URL + "/api/v0/quizzes?"
+                + "player=" + encodedPlayer
+                + "&account=" + encodedAccount;
+
+        HttpResponse response = httpClient.send(
+                HttpRequest.newBuilder()
+                        .uri(URI.create(fullUrl))
+                        .headers(
+                                "api-key", API_KEY
+                        )
+                        .GET()
+                        .build(),
+                HttpResponse.BodyHandlers.ofString()
+        );
+        super.logInformation("Status Code: " + response.statusCode());
+        super.logInformation("Response Body: " + response.body());
+        return response;
+    }
+
+    @Override
+    public HttpResponse getResultOfQuizz(String quizz, String account) throws IOException, InterruptedException {
+        String encodedAccount = URLEncoder.encode(account, "UTF-8");
+        String encodedQuizz = URLEncoder.encode(quizz, "UTF-8");
+
+        String fullUrl = API_URL + "/api/v0/quizzes/"
+                + encodedQuizz + "/result?"
+                + "&account=" + encodedAccount;
+
+        HttpResponse response = httpClient.send(
+                HttpRequest.newBuilder()
+                        .uri(URI.create(fullUrl))
+                        .headers(
+                                "api-key", API_KEY
+                        )
+                        .GET()
+                        .build(),
+                HttpResponse.BodyHandlers.ofString()
+        );
+        super.logInformation("Status Code: " + response.statusCode());
+        super.logInformation("Response Body: " + response.body());
+        return response;
+    }
+
+    @Override
+    public HttpResponse getResultOfQuizzByPlayer(String quizz, String account, String player) throws IOException, InterruptedException {
+        String encodedQuizz = URLEncoder.encode(quizz, "UTF-8");
+        String encodedAccount = URLEncoder.encode(account, "UTF-8");
+        String encodedPlayer = URLEncoder.encode(player, "UTF-8");
+
+        String fullUrl = API_URL + "/api/v0/quizzes/"
+                + encodedQuizz + "/result?"
+                + "account=" + encodedAccount
+                + "&player=" + encodedPlayer;
+
+        HttpResponse response = httpClient.send(
+                HttpRequest.newBuilder()
+                        .uri(URI.create(fullUrl))
+                        .headers(
+                                "api-key", API_KEY
+                        )
+                        .GET()
+                        .build(),
+                HttpResponse.BodyHandlers.ofString()
+        );
+        super.logInformation("Status Code: " + response.statusCode());
+        super.logInformation("Response Body: " + response.body());
+        return response;
     }
 
     @Override
--- lib/src/main/java/fr/parisNanterre/iqplaylib/gamelayer/api/IGameLayerQuizzService.java
+++ lib/src/main/java/fr/parisNanterre/iqplaylib/gamelayer/api/IGameLayerQuizzService.java
@@ -1,12 +1,15 @@
 package fr.parisnanterre.iqplaylib.gamelayer.api;
 
+import java.io.IOException;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 
 public interface IGameLayerQuizzService {
-    HttpResponse getQuizzById(String quizz, String account);
-    HttpResponse getQuizzes(String account);
-    HttpResponse getResultOfQuizz(String quizz, String account);
+    HttpResponse getQuizzById(String quizz, String account) throws IOException, InterruptedException;
+    HttpResponse getQuizzes(String account) throws IOException, InterruptedException;
+    HttpResponse getQuizzesByPlayer(String player, String account) throws IOException, InterruptedException;
+    HttpResponse getResultOfQuizz(String quizz, String account) throws IOException, InterruptedException;
+    HttpResponse getResultOfQuizzByPlayer(String quizz, String account, String player) throws IOException, InterruptedException;
     HttpResponse startAQuizz(String quizz, String account, String player);
     HttpResponse completeQuizz(String quizz, String account, String player, ArrayList<String> answers);
 }
