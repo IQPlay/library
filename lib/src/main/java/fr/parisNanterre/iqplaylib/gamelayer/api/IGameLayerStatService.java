95ec8971598bad88d9d8e5b40ee92fe95f7006b5
Sami Letellier <42002121@parisnanterre.fr>
2025-03-16 05:10:55 +0100

    Erreur 500 côté gamelayer, impossible d'implementer les méthodes liéss aux analytics

--- lib/src/main/java/fr/parisNanterre/iqplaylib/gamelayer/api/IGameLayerStatService.java
+++ lib/src/main/java/fr/parisNanterre/iqplaylib/gamelayer/api/IGameLayerStatService.java
@@ -2,6 +2,7 @@ package fr.parisnanterre.iqplaylib.gamelayer.api;
 
 import java.io.IOException;
 
+// Erreur 500 côté serveur de GameLayer
 public interface IGameLayerStatService {
     String getAnalytics() throws IOException;
 
