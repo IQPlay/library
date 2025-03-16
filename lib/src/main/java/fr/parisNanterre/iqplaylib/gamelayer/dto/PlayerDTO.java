2b60c56658ca1a2d2766de7325a532723e39fb85
Sami Letellier <42002121@parisnanterre.fr>
2025-03-16 02:40:00 +0100

    Ajout d'un constructeur pour les recherches by ID + ajout du champ account

--- lib/src/main/java/fr/parisNanterre/iqplaylib/gamelayer/dto/PlayerDTO.java
+++ lib/src/main/java/fr/parisNanterre/iqplaylib/gamelayer/dto/PlayerDTO.java
@@ -4,9 +4,6 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 /**
  * DTO pour la création d'un joueur dans GameLayer.
@@ -30,6 +27,12 @@ public class PlayerDTO {
     @NotBlank(message = "Le nom du niveau est obligatoire")
     private String levelName;
 
+    @NotBlank(message = "Le joueur doit avoir un identifiant")
+    private String player;
+
+    @NotBlank(message = "Le joueur doit avoir un compte")
+    private String account;
+
     //  Constructeurs
     public PlayerDTO() {}
 
@@ -40,6 +43,11 @@ public class PlayerDTO {
         this.credits = credits;
         this.levelId = levelId;
         this.levelName = levelName;
+    }
+
+    public PlayerDTO(String player, String account) {
+        this.player = player;
+        this.account = account;
     }
 
     //  Getters et Setters
@@ -60,4 +68,20 @@ public class PlayerDTO {
 
     public String getLevelName() { return levelName; }
     public void setLevelName(String levelName) { this.levelName = levelName; }
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(String player) {
+        this.player = player;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
 }
