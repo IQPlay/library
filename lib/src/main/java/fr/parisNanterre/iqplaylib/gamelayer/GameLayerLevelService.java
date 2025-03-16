901b198484e8852c068214531fe50a5ce30b9ca4
Sami Letellier <42002121@parisnanterre.fr>
2025-03-16 04:58:15 +0100

    Ajout du service lié au level: implémentation des méthodes getLevel et getAllLevels

---
+++ lib/src/main/java/fr/parisNanterre/iqplaylib/gamelayer/GameLayerLevelService.java
@@ -0,0 +1,57 @@
+package fr.parisnanterre.iqplaylib.gamelayer;
+
+import fr.parisnanterre.iqplaylib.gamelayer.api.IGameLayerLevelService;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class GameLayerLevelService extends GameLayerService implements IGameLayerLevelService {
+
+    @Override
+    public void getLevelById(String levelId, String account) throws IOException, InterruptedException {
+        String encodedEventId = URLEncoder.encode(levelId, "UTF-8");
+        String encodedAccount = URLEncoder.encode(account, "UTF-8");
+
+        String fullUrl = API_URL + "/api/v0/levels/"
+                + encodedEventId
+                + "?account=" + encodedAccount
+                + "&language=fr";
+
+        HttpResponse response = httpClient.send(
+                HttpRequest.newBuilder()
+                        .uri(URI.create(fullUrl))
+                        .headers(
+                                "api-key", API_KEY
+                        )
+                        .GET()
+                        .build(),
+                HttpResponse.BodyHandlers.ofString()
+        );
+        System.out.println("Status Code: " + response.statusCode());
+        System.out.println("Response Body: " + response.body());
+    }
+
+    @Override
+    public void getAllLevels(String account) throws IOException, InterruptedException {
+        String encodedAccount = URLEncoder.encode(account, "UTF-8");
+
+        String fullUrl = API_URL + "/api/v0/levels" + "?account=" + encodedAccount;
+
+        HttpResponse response = httpClient.send(
+                HttpRequest.newBuilder()
+                        .uri(URI.create(fullUrl))
+                        .headers(
+                                "api-key", API_KEY
+                        )
+                        .GET()
+                        .build(),
+                HttpResponse.BodyHandlers.ofString()
+        );
+        System.out.println("Status Code: " + response.statusCode());
+        System.out.println("Response Body: " + response.body());
+    }
+
+}
--- lib/src/main/java/fr/parisNanterre/iqplaylib/gamelayer/api/IGameLayerLevelService.java
+++ lib/src/main/java/fr/parisNanterre/iqplaylib/gamelayer/api/IGameLayerLevelService.java
@@ -1,4 +1,8 @@
 package fr.parisnanterre.iqplaylib.gamelayer.api;
 
-public class IGameLayerLevelService {
+import java.io.IOException;
+
+public interface IGameLayerLevelService {
+    void getLevelById(String levelId, String account) throws IOException, InterruptedException;
+    void getAllLevels(String account) throws IOException, InterruptedException;
 }
